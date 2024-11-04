c705de6c1225dc7612bab1080f58fb5b7603b94a
asleep <2677912369@qq.com>
2024-11-04 15:20:37 +0800

    成功实现双页面

--- resource.h
+++ resource.h
@@ -38,6 +38,7 @@
 #define IDC_LaserType                   1035
 #define IDC_EDIT2                       1036
 #define IDC_COMBO2                      1037
+#define IDC_show_Sample                 1038
 
 // Next default values for new objects
 // 
@@ -45,7 +46,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1038
+#define _APS_NEXT_CONTROL_VALUE         1039
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
